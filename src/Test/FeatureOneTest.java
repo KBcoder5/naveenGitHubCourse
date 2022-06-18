af3280d948bc501cd35448439f55dbe25a100c5d
Kbcoder5 <kush.beriwal@gmail.com>
2022-06-18 22:48:04 +0530

    Dev1 FeatureOneTest changes

--- src/Test/FeatureOneTest.java
+++ src/Test/FeatureOneTest.java
@@ -3,8 +3,9 @@ package Test;
 public class FeatureOneTest {
 	
 	public void featureTest(){
-		System.out.println("Feature One Test Start Dev2");
-		System.out.println("Feature One Test Ends Dev2");
+
+		System.out.println("Feature One Test Start Dev1");
+		System.out.println("Feature One Test Ends Dev1");
 	}
 
 }
