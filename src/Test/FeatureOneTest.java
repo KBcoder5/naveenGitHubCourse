d7fef9a4e17cada83b3c06fd4ad80df316e20087
Kbcoder5 <kush.beriwal@gmail.com>
2022-06-18 23:36:12 +0530

    Revert "Dev1 FeatureOneTest changes"
    This reverts commit af3280d948bc501cd35448439f55dbe25a100c5d.

--- src/Test/FeatureOneTest.java
+++ src/Test/FeatureOneTest.java
@@ -3,9 +3,8 @@ package Test;
 public class FeatureOneTest {
 	
 	public void featureTest(){
-
-		System.out.println("Feature One Test Start Dev1");
-		System.out.println("Feature One Test Ends Dev1");
+		System.out.println("Feature One Test Start Dev2");
+		System.out.println("Feature One Test Ends Dev2");
 	}
 
 }
