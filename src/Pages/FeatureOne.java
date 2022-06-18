a431bbc75c336711ff384c4173f489f1401e81c2
KBcoder5 <41131218+KBcoder5@users.noreply.github.com>
2022-06-18 22:26:35 +0530

    Create FeatureOne.java

---
+++ src/Pages/FeatureOne.java
@@ -0,0 +1,12 @@
+package Pages;
+
+public class FeatureOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Feature One Tag Example Dev2");
+		System.out.print("Feature One Tag Example Two Dev2");
+		System.out.print("Feature One Tag Example Three Dev2");
+	}
+
+}
