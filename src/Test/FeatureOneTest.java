dd8c3b3487adf3212efe0d9ec561be85673ef300
KBcoder5 <41131218+KBcoder5@users.noreply.github.com>
2022-06-18 22:31:56 +0530

    Create FeatureOneTest.java

---
+++ src/Test/FeatureOneTest.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class FeatureOneTest {
+	
+	public void featureTest(){
+		System.out.println("Feature One Test Start Dev2");
+		System.out.println("Feature One Test Ends Dev2");
+	}
+
+}
